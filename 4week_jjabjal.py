87f8a41e6d42fc0ce89b870c277d5eff566ababb
kbs3982 <kbs3982@naver.com>
2020-07-31 22:51:17 +0900

    짭잘한맛 숙제
    끝

--- 4week_jjabjal.py
+++ 4week_jjabjal.py
@@ -1,5 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
+from pymongo import MongoClient
+
+client = MongoClient('localhost', 27017)
+db = client.dbsparta
+
 
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
 
@@ -18,3 +23,13 @@ for tr in trs:
     artist = tr.select_one('td.info > a.artist.ellipsis').text
 
     print(rank,name,artist)
+
+    music = {
+        'rank': rank,
+        'name': name,
+        'artist': artist
+    }
+
+    db.music.insert_one(music)
+
+    
